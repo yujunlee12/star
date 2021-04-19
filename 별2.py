1cb43fb9b28f0854a008e2fa4882f084fcf806ef
yujunlee12 <78995529+yujunlee12@users.noreply.github.com>
2021-04-19 16:30:04 +0900

    Update 별2.py

--- 별2.py
+++ 별2.py
@@ -12,16 +12,4 @@ for i in range(a-d):
         print("*",end='')
     print("")
     c=c+2
-for m in range(a-(d+1)):
-    for o in range(d-k):
-        print(" ",end='')
-    for p in range(5):
-        print("*",end='')
-    print("")
-    k=k+1
-    c=c-2
-    
-    
-    
-             
-    
+ 
